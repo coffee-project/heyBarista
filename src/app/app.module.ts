95e7376e6789e123f923b411ca301ad416a503c8
rafszul <rafszul@gmail.com>
2017-04-28 02:18:13 +0100

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,16 @@ import { AngularFireModule } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
